0c4aa3c17656dbc40def420b0692d60470ed248a
GitBurger21 <163922158+GitBurger21@users.noreply.github.com>
2024-03-18 16:27:32 -0700

    Add files via upload
    Second Upload!

---
+++ Assighnment 1 2.16 - Copy.py
@@ -0,0 +1,10 @@
+#Anthony Burger 007196196
+
+#Get User Input
+userinput = input('Enter Five Digit positive Integer: ')
+
+#Format It
+print (userinput [0], ' ', userinput [1], ' ', userinput [2], ' ',
+       userinput [3], ' ', userinput [4])
+
+
